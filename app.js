cb558250fb5d2753f946762c04f2d0b6e81ba0bc
Do Tai <end_lesslove2012@yahoo.com>
2018-02-19 20:39:43 -0800

    Deploy test

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/user', user);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 app.listen(port, () => {
   console.log('Server Started');
