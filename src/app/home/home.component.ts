84efedc4981ea392707439706ea5d12a3827c70c
Do Tai <end_lesslove2012@yahoo.com>
2018-02-22 11:01:18 -0800

    Fix UI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -32,7 +32,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
       }
       const balance = await this.web3.instance.eth.getBalance(this.web3.Contract().options.address);
       this.totalPrize = await this.web3.instance.utils.fromWei(balance, 'ether');
-      if (this.totalPrize >= 2) {
+      if (this.totalPrize >= 0.05) {
         this.showPrize = true;
       }
     } catch (error) {
