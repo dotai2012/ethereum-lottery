a6da46a24aba137030e07836f4b90b6c054938a0
Do Tai <end_lesslove2012@yahoo.com>
2018-02-21 16:06:21 -0800

    Port

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connection.on('error', (error) => {
 });
 
 const app = express();
-const port = 3000;
+const port = process.env.port || 80;
 
 app.use(compression({ level: 9 }));
 app.use(cors());
